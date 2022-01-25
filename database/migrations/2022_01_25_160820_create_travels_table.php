f2bf0e12a50fb4b5621bbcbd843eec345fe6ca98
matteofab10 <matteofabio10@gmail.com>
2022-01-25 17:59:24 +0100

    faccio la migrazione dei dati

---
+++ database/migrations/2022_01_25_160820_create_travels_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 200);
+            $table->tinyInteger('duration');
+            $table->decimal('price', 5, 2);
+            $table->string('type', 50);
+            $table->date('available_until');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
