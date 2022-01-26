f6a5fc72a8741c82e6bee19fddb38de16be7947f
matteofab10 <matteofabio10@gmail.com>
2022-01-26 09:47:09 +0100

    completo esercizio

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -10,9 +10,6 @@
     <li class="nav-item">
       <a class="nav-link" href="#">Link</a>
     </li>
-    <li class="nav-item">
-      <a class="nav-link disabled">Disabled</a>
-    </li>
   </ul>
 
 </header>
